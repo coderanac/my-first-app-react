8666a78b34792222a4b25654f73e30ebe67949ed
coderanac <coder.anac@gmail.com>
2020-06-25 19:27:03 -0300

    :rocket: deploy

---
+++ precache-manifest.18dadc65b710962bce8caa1cfaf0cfb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0f834888838f2b4eb6437827a5b2ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452bd9b5287e274c74d2",
+    "url": "/static/css/main.a0480767.chunk.css"
+  },
+  {
+    "revision": "a360e8d51761942eadd8",
+    "url": "/static/js/2.135607f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.135607f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452bd9b5287e274c74d2",
+    "url": "/static/js/main.db7d9f92.chunk.js"
+  },
+  {
+    "revision": "f64bbedbc980241ccee0",
+    "url": "/static/js/runtime-main.18399cad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
